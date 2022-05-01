5a37d3b60807d65bec30bef3dae30ceb4a0b3bb2
herwintorres <hertosan29@gmail.com>
2022-04-30 20:54:46 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// funciones declarativas
+function saludar(nombre){
+    console.log(`Hola ${nombre}`)
+}
+saludar("herwin")
+
+//Expresión de funciones
+
+var funcion = function(nombre){
+    console.log(`Hola ${nombre}`)
+}
+
+funcion("Alexandra");
